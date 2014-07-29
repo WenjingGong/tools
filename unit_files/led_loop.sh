d91575cc53e796a671082f21ac892945c4c96430
WenjingGong <wenjing@leeo.com>
2014-07-29 10:19:00 -0700

    unit_files commit 1

--- unit_files/led_loop.sh
+++ unit_files/led_loop.sh
@@ -3,17 +3,14 @@
 var=1
 while [ "$var" -gt 0 ]
 do
-/usr/lib/leeoalpha/sensors/led_off.sh
-i2cset -y 1 0x4c 0x02 0x0F0F w
-sleep 1
-/usr/lib/leeoalpha/sensors/led_off.sh
+/usr/lib/leeonl/sensors/led_off.sh
 i2cset -y 1 0x4c 0x03 0x7878 w
 sleep 1
-/usr/lib/leeoalpha/sensors/led_off.sh
+/usr/lib/leeonl/sensors/led_off.sh
 i2cset -y 1 0x4c 0x04 0xD2D2 w
 sleep 1
-/usr/lib/leeoalpha/sensors/led_off.sh
+/usr/lib/leeonl/sensors/led_off.sh
 i2cset -y 1 0x4c 0x05 0xFFFF w
 sleep 1
-/usr/lib/leeoalpha/sensors/led_off.sh
+/usr/lib/leeonl/sensors/led_off.sh
 done
